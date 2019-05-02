be1e3b3880d17e57c350f68e58b56a66f780ff00
Hogyun Kim <hogyun3709@gmail.com>
2019-05-03 01:36:59 +0900

    Create test case: check runtime efficiency

--- functionalProgrammingAdvance/range.js
+++ functionalProgrammingAdvance/range.js
@@ -1,4 +1,15 @@
 const add = (a, b) => a + b;
+const _reduce = (fns, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = fns(acc, a);
+  }
+  return acc;
+};
+
 const range = l => {
   let i = -1;
   let res = [];
@@ -14,21 +25,28 @@ var list = range(4);
 
 console.log(list.reduce(add));
 
+// L.range 는 array 를 만들지 않고 값을 꺼내기만한다.
 
+const L = {};
 
-
-const L = {}
-const add = (a, b) => a + b;
-L.range = l => {
+L.range = function*(l) {
   let i = -1;
-  let res = [];
   while (++i < l) {
-    res.push(i);
+    yield i;
   }
-  return res;
 };
-
 
 var list = L.range(4);
 
-console.log(list.reduce(add));
+console.log(list);
+
+console.log(_reduce(add, list));
+
+function test(name, time, f) {
+  console.time(name);
+  while (time--) f();
+  console.timeEnd(name);
+}
+
+test("L.range", 10, () => _reduce(add, L.range(10000000)));
+test("range", 10, () => _reduce(add, range(10000000)));
