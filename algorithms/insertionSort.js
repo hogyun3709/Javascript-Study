fe4090374fa38c2bcebbac2a36186ddfac7497bc
hogyun3709 <hogyun3709@gmail.com>
2019-05-17 11:27:04 +0900

    Update insertionSort.js

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -2,13 +2,15 @@ insertionSort = (array) => {
   for (outer = 1; outer < array.length; outer++) {
     for (inner = 0; inner < outer; inner++) {
       console.log(array.join(' '))
+      // if inner value is smaller, only outer and inner counter increase
       if (array[outer] < array[inner]) {
         //eject smaller(outer) value in tmp array
         const [element] = array.splice(outer, 1)
-        console.log(element);
+        console.log("Element: " + element);
         //outer and inner are index of array
-        console.log(outer);
-        console.log(inner);
+        console.log("Outer: " + outer);
+        console.log("Inner: " + inner);
+        //Following line: remove 0 element from index 0(inner), insert element at the index(inner)
         array.splice(inner, 0, element)
 
       }
