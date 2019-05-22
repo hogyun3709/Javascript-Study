289b19fb940405737aa7d2768d9101dbdeba6e2f
hogyun3709 <hogyun3709@gmail.com>
2019-05-22 21:47:23 +0900

    update mergeSort.js

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -3,11 +3,11 @@ mergeSort = (array) => {
     return array
   }
   const mid = Math.floor(array.length / 2)
-  console.log("Mid: " + mid);
+  // console.log("Mid: " + mid);
   const arrayA = array.slice(0, mid)
   const arrayB = array.slice(mid)
-  console.log("Array of A: " + arrayA);
-  console.log("Array of B: " + arrayB);
+  // console.log("Array of A: " + arrayA);
+  // console.log("Array of B: " + arrayB);
   // recursively call mergeSort(arrayA)
   // mergeSort(arrayA) calls first, mergeSort(arrayB) calls next,
   // while sort(mergeSort(arrayA)) executes, it creates new sub arrayA and arrayB data sets to sort the values
@@ -15,27 +15,17 @@ mergeSort = (array) => {
 }
 
 sort = (arrayA, arrayB) => {
-  console.log("Values are ready: " +  "ArrayA: " + arrayA + "," + " arrayB: "+ arrayB);
+  // console.log("Values are ready: " +  "ArrayA: " + arrayA + "," + " arrayB: "+ arrayB);
   const sorted = []
-
   while (arrayA.length && arrayB.length) {
-    if (arrayA[0] <= arrayB[0]) {
-      sorted.push(arrayA.shift())
-    } else {
-      sorted.push(arrayB.shift())
-    }
-    console.log("Instant Sorted: " + sorted);
+    arrayA[0] <= arrayB[0] ? sorted.push(arrayA.shift()) : sorted.push(arrayB.shift())
+    // console.log("Instant Sorted: " + sorted);
   }
-  const output = [
-    ...sorted,
-    ...arrayA,
-    ...arrayB
-  ]
-  console.log("Sorted: " + sorted);
-  console.log("arrayA: " + arrayA);
-  console.log("arrayB: " + arrayB);
-  console.log("Sorted Output: " + output)
-
+  const output = [...sorted,...arrayA, ...arrayB]
+  // console.log("Sorted: " + sorted);
+  // console.log("arrayA: " + arrayA);
+  // console.log("arrayB: " + arrayB);
+  // console.log("Sorted Output: " + output)
   return output
 }
 
