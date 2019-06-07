6fe486e41396a1744ee6a413ca9a9e14c4a14fa9
Hogyun Kim <hogyun3709@gmail.com>
2019-06-07 11:08:59 +0900

    Use map funs to allocate the value to var after change the value

--- functionalProgrammingApplication/addOdd.js
+++ functionalProgrammingApplication/addOdd.js
@@ -15,14 +15,13 @@ function f1(limit, list) {
 f1(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
 // iterable / lisp
-function f2(limit, list) {
+function f3(limit, list) {
   let acc = 0;
-  for (const a of L.filter(a => a % 2, list)) {
-    const b = a * a;
+  for (const a of L.map(a => a * a, L.filter(a => a % 2, list))) {
     acc += b;
     if (--limit == 0) break;
   }
   console.log(acc);
 }
 
-f2(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+f3(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
