fa559c47082e2a0aabb826e2bfd9a7a8ab22fa90
Hogyun Kim <hogyun3709@gmail.com>
2019-06-07 10:56:46 +0900

    Convert imperative to iterable programming

--- functionalProgrammingApplication/addOdd.js
+++ functionalProgrammingApplication/addOdd.js
@@ -1,15 +1,28 @@
 //imperative
 
-function f1(limit, list){
+function f1(limit, list) {
   let acc = 0;
   for (const a of list) {
     if (a % 2) {
       const b = a * a;
-      acc += b
-      if( --limit == 0) break;
+      acc += b;
+      if (--limit == 0) break;
     }
   }
   console.log(acc);
 }
 
-f1(3, [1,2,3,4,5,6,7,8,9]);
+f1(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// iterable / lisp
+function f2(limit, list) {
+  let acc = 0;
+  for (const a of L.filter(a => a % 2, list)) {
+    const b = a * a;
+    acc += b;
+    if (--limit == 0) break;
+  }
+  console.log(acc);
+}
+
+f2(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
