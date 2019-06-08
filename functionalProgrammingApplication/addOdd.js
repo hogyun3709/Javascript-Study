1fc0f66d08babbba7a15f596d89f4794356daaba
Hogyun Kim <hogyun3709@gmail.com>
2019-06-08 17:01:08 +0900

    Refactor break statement by using take funs

--- functionalProgrammingApplication/addOdd.js
+++ functionalProgrammingApplication/addOdd.js
@@ -25,3 +25,13 @@ function f3(limit, list) {
 }
 
 f3(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// L.map 에서 몇개의 값을 받아와도 limit 값만 next 하게됨
+function f4(limit, list){
+  let acc = 0;
+  for (const a of L.take(limit, L.map( a => a * a, L.filter(a => a % 2, list))))  {
+    acc += a;
+  }
+  console.log(acc);
+}
+f4(3, [1,2,3,4,5,6,7,8,9])
