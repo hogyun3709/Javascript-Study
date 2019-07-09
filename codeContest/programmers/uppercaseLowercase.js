5261260a67978a1d129a0afad18f3ee02e288fa2
Hogyun Kim <hogyun3709@gmail.com>
2019-07-09 20:40:48 +0900

    Alogrithm

---
+++ codeContest/programmers/uppercaseLowercase.js
@@ -0,0 +1,17 @@
+const s = 'try hello world'
+
+function solution(s) {
+    var answer = '';
+    /* Need to split each single string due to evaulation*/
+    var tmp = s.split('');
+    for(let i = 0; i < s.length; i++){
+        if(i % 2 == 0){
+            answer += tmp[i].toUpperCase()
+        } else if (i % 2 === 1){
+            answer += tmp[i].toLowerCase()
+        }
+    }
+    return answer;
+}
+
+console.log(solution(s));
