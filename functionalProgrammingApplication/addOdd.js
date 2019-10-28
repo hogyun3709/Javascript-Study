0a8865244c13e5ced96ab5d7c3ceaf62a6e46f00
Hogyun Kim <hogyun3709@gmail.com>
2019-10-28 13:46:49 +0900

    이터러블프로그래밍:while 을 range 로

--- functionalProgrammingApplication/addOdd.js
+++ functionalProgrammingApplication/addOdd.js
@@ -36,7 +36,7 @@ function f4(limit, list){
 }
 f4(3, [1,2,3,4,5,6,7,8,9])
 
-// Reduce 로 합산 하기 
+// Reduce 로 합산 하기
 const add = (a,b) => a + b
 function f5(limit, list){
   console.log(
@@ -57,3 +57,27 @@ function f5(limit, list){
   )
 }
 f5(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// while 을 사용한 평범한 방법, 명령형의 스마트한 버전, range로 변환
+
+function f6(end){
+  let i = 0;
+  while (i < end){
+    console.log(i);
+    i++
+  }
+}
+
+function f6_1(end){
+  let i = 1;
+  while (i < end){
+    console.log(i)
+    i += 2;
+  }
+}
+function f6_2(end){
+  _.each(console.log, L.range(1, end, 2));
+}
+f6(10);
+f6_1(10);
+f6_2(10);
