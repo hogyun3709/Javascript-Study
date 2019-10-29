5115154333064c1b811311c8c7ca25066327c299
Hogyun Kim <hogyun3709@gmail.com>
2019-10-29 19:23:26 +0900

    이터러블 프로그래밍: 구구단(이터러블식)

--- functionalProgrammingApplication/addOdd.js
+++ functionalProgrammingApplication/addOdd.js
@@ -101,3 +101,16 @@ _.go(
   console.log
 
 )
+
+// 구구단 만들기
+const join = sep => _.reduce((a, b) => `${a}${sep}${b}`);
+_.go(
+  L.range(2,10),
+  L.map(a => _.go(
+    L.range(1, 10),
+    L.map( b => `${a}x${b}=${a*b}`),
+    join(`\n`)
+  )),
+  join(`\n\n`),
+  console.log
+)
