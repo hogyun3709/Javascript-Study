84f9f0171c556eb8446a13191a77ea1c5a4f9948
hogyun3709 <hogyun3709@gmail.com>
2020-07-22 14:59:51 +0900

    Entries = 객체를 itreable

--- functionalProgrammingApplication/handle_object_to_iterable_programming.js
+++ functionalProgrammingApplication/handle_object_to_iterable_programming.js
@@ -1,7 +1,7 @@
 <script>
  console.log(Object.values(obj1)) /* (4) [1,2,3,4]*/
 
- /* Values 를 구현해보자 */
+ /* 1. Values 를 구현해보자 */
 
  L.values = function *(obj){
    for (const k in obj){
@@ -33,6 +33,28 @@
    L.map(a => a + 10),
    L.take(2),
    _.reduce((a, b) => a + b),
+   console.log
+ )
+
+ /* 2. Entries 를 구현해보자 */
+
+ /* key 와 value 에 모두 yield*/
+
+ L.entries = function *(obj) {
+   for (const k in obj){
+     yield [k, obj[k]];
+   }
+ }
+
+ /* entries 를 활용하여 특정 function 을 적용한 key 와 value를 하나의 object 로 assign */
+ /* Obj array 에서 홀수의 value를 가진 key value 쌍을 하나의 오브젝트로 */
+ /* Key Point = array 값이 어떻든 iterable 하게 프로그래밍이 가능*/
+ _.go(
+   obj1,
+   L.entries,
+   L.filter(([_, v]) => v % 2),
+   L.map(([k, v]) => ({[k]: v})),
+   _.reduce(Object.assign),
    console.log
  )
 </script>
